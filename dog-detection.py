75c59dcd5ffed2b6cc077ba778455e8337211520
km7872 <km7872@rit.edu>
2025-02-05 17:05:56 -0500

    minor issues

--- dog-detection.py
+++ dog-detection.py
@@ -4,13 +4,13 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-model_path = '' # model file goes here
+model_path = 'model_path' # model file goes here
 
 # Load the input image from an image file.
-numpy_image = cv2.imread('img1.jpg')  # Make sure the path is correct
+numpy_image = cv2.imread('img1.jpg')  #
 
 # Convert the image from BGR (OpenCV default) to RGB.
-numpy_image = cv2.cvtColor(numpy_image, cv2.COLOR_BGR2RGB)
+# numpy_image = cv2.cvtColor(numpy_image, cv2.COLOR_BGR2RGB)
 
 # Load the input image from a numpy array.
 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=numpy_image)
@@ -32,22 +32,22 @@ with ObjectDetector.create_from_options(options) as detector:
     for detection in detection_result.detections:
         # Get bounding box and convert it to pixel coordinates.
         # print(detection)
-        if detection.categories[0].category_name=='dog':
-            bbox = detection.bounding_box
-            x = int(bbox.origin_x)
-            y = int(bbox.origin_y)
-            w = int(bbox.width)
-            h = int(bbox.height)
 
-            # Draw a rectangle around the detected object.
-            cv2.rectangle(numpy_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        bbox = detection.bounding_box
+        x = int(bbox.origin_x)
+        y = int(bbox.origin_y)
+        w = int(bbox.width)
+        h = int(bbox.height)
+
+        # Draw a rectangle around the detected object.
+        cv2.rectangle(numpy_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # # Show the image with the bounding boxes.
     # cv2.imshow('Object Detection', numpy_image)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    # TODO : remove discoloration
+
     # Convert the image to RGB for displaying with matplotlib.
     numpy_image_rgb_display = cv2.cvtColor(numpy_image, cv2.COLOR_BGR2RGB)
 
